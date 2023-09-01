7d1ee48bfc599d6a1b479b906c409c831c31e91a
Luka Verzijl <redlucraft@gmail.com>
2023-09-01 11:35:44 +0200

    moved apikey to env and generated new apikey

--- src/prismic.ts
+++ src/prismic.ts
@@ -2,10 +2,11 @@ import * as prismic from '@prismicio/client'
 
 // Fill in your repository name
 export const repositoryName = 'lukaportfoliocms'
+const apikey = import.meta.env.VITE_PRISMICAPI
 
 export const client = prismic.createClient(repositoryName, {
   // If your repository is private, add an access token
-  accessToken: 'MC5aTy1Pa2hJQUFDRUFwYWpo.77-9Le-_vVAw77-9WQg177-977-977-9Hx0M77-9XTjvv73vv70sORVt77-9UGPvv73vv70_SRY',
+  accessToken: apikey,
 
   // This defines how you will structure URL paths in your project.
   // Update the types to match the custom types in your project, and edit

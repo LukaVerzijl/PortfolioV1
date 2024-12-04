ad0accc7d3e2ee64c1c6d6ac1896f390abfda25a
Luka <redlucraft@gmail.com>
2024-12-04 22:32:01 +0100

    tsconf

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,5 +8,8 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
-  }
+  },
+  define: { 
+    'import.meta.vitest': 'undefined', 
+  }, 
 })

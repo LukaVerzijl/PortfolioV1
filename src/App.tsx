e23bf9f8d46ee30a283a7125ccec5fc39065d910
Luka Verzijl <lukaverzijl@hotmail.com>
2025-02-13 14:03:02 +0100

    build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import WebTree from "./components/WebTree";
 import ProjectPageTree from "./components/ProjectPageTree";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
   const location = useLocation();
-
+  const queryClient = new QueryClient();
   return (
-    <Routes>
-      <Route path="/" key={"home"} element={<WebTree />} />
-      <Route path="/project/:name" element={<ProjectPageTree />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" key={"home"} element={<WebTree />} />
+        <Route path="/project/:name" element={<ProjectPageTree />} />
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
